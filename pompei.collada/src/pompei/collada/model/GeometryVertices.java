840e7942077a328752c92637bdc97b17283470fc
pompei <pompei@pompei>
2014-10-11 14:12:35 +0600

    Добавлен ещё один метод

---
+++ pompei.collada/src/pompei/collada/model/GeometryVertices.java
@@ -0,0 +1,22 @@
+package pompei.collada.model;
+
+public class GeometryVertices implements XmlAppender, HasId {
+  public String id, name;
+  public VerticesMesh verticesMesh;
+  
+  @Override
+  public String getId() {
+    return id;
+  }
+  
+  @Override
+  public void append(StringBuilder sb) {
+    sb.append("<geometry");
+    U.appendAny(sb, "id", id);
+    U.appendAny(sb, "name", name);
+    sb.append('>');
+    U.append(sb, verticesMesh);
+    sb.append("</geometry>");
+  }
+  
+}
--- pompei.collada/src/pompei/collada/model/VerticesMesh.java
+++ pompei.collada/src/pompei/collada/model/VerticesMesh.java
@@ -1,5 +1,19 @@
 package pompei.collada.model;
 
-public class VerticesMesh {
+public class VerticesMesh implements XmlAppender {
+  
+  public FloatSource positionSource, normalSource;
+  public Vertices vertices;
+  public Polylist polylist;
+  
+  @Override
+  public void append(StringBuilder sb) {
+    sb.append("<mesh>");
+    U.append(sb, positionSource);
+    U.append(sb, normalSource);
+    U.append(sb, vertices);
+    U.append(sb, polylist);
+    sb.append("</mesh>");
+  }
   
 }
